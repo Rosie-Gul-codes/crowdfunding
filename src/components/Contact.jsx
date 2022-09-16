39a3e9a184b606d86f0adf3504802ecfd330ff1c
Rosie-Gul-codes <guldidar.kizil@gmail.com>
2022-09-16 21:33:25 +1000

    class error is fixed
    Class replaced with className

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,26 @@
 
 function Contact () {
     return (
-        <div class="container">
+        <div className="container">
                             <form class="form-style" action="https://formspree.io/f/xvolwjwz"
                             method="POST">
 
-                        <div class="input-field">
+                        <div className="input-field">
                             <label htmlFor="fname">First Name</label>
                             <input type="text" id="fname" name="firstname" placeholder="Your name.." />
                         </div>
 
-                        <div class="input-field">
+                        <div className="input-field">
                             <label htmlFor="lname">Last Name</label>
                             <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
                         </div>
 
-                        <div class="input-field">
+                        <div className="input-field">
                             <label htmlFor="subject">Subject</label>
                             <textarea id="subject" name="subject" placeholder="Write something.." style={{height:"200px"}} />
                         </div>      
 
-                            <input class="submit-button" type="submit" value="Submit" />
+                            <input className="submit-button" type="submit" value="Submit" />
                             </form>
 
                         </div>
